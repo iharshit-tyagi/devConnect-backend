85a8f58a9a951ef121a14eb9eb0b730be4aeab82
Harshit <harshitt52@gmail.com>
2025-01-17 17:09:23 +0530

    Added Middleware to Validate input for signup/signin

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,13 +2,43 @@ import { Router } from "express"
 
 const userRoute=Router();
 
-userRoute.post('/signin',(req,res)=>{
+const checkLoginBody=(req,res,next)=>{
+  
+    const email = req?.body?.email;
+    const password= req?.body?.password;
+    
+    if( !email || !password){
+        res.status(400).json({
+            success: false,
+            message: 'Email or Password is missing'
+        });
+        return;
+    }
+  next();
+   
+}
+const checkLogoutBody=(req,res,next)=>{
+    const email = req?.body?.email;
+    const password= req?.body?.password;
+    const name = req?.body?.name;
+    if( !email || !password || !name){
+        res.status(400).json({
+            success: false,
+            message: 'Invalid Input'
+        });
+        return;
+    }
+  next();
+   
+}
+
+userRoute.post('/signin',checkLoginBody,(req,res)=>{
 res.status(200).json({
     success: true,
     result: 'Logged in'
 })
 })
-userRoute.post('/signup',(req,res)=>{
+userRoute.post('/signup',checkLogoutBody,(req,res)=>{
 res.status(200).json({
     success: true,
     result: 'Signed Up'
